be09fcf0f0477891bc708d19039ab88298ea106a
Irina <balkarovairina@gmail.com>
2021-07-11 20:15:11 +0200

    fix: banner component

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,16 +1,14 @@
-import banner from '../image/banner.png';
 import styled from 'styled-components';
+import banner from '../image/banner.png';
 
-const BannerStyled = styled.div`
+export const Banner = styled.div`
     background-image: linear-gradient(
             0deg,
             rgba(238, 238, 238, 0.2),
             rgba(1, 93, 0, 0.2)
         ),
         url(${banner});
-    height: 150px;
+    height: 200px;
     background-repeat: no-repeat;
     background-size: cover;
 `;
-
-export const Banner = () => <BannerStyled />;
